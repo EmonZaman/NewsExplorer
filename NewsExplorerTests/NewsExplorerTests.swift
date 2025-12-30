99d90a2ffd8b08b02f3b81de0b3170b23f3184fd
EmonZaman <aagontukemon@gmail.com>
2025-12-30 23:09:30 +0600

    initail project setup

---
+++ NewsExplorerTests/NewsExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsExplorerTests.swift
+//  NewsExplorerTests
+//
+//  Created by Aagontuk on 12/30/25.
+//
+
+import Testing
+@testable import NewsExplorer
+
+struct NewsExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
